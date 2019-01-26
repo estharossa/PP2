34c06863e3a3c7b132eb7dbf918ce43a3e1221af
estharossa <danik.encore@gmail.com>
2019-01-26 10:30:54 +0600

    asd

---
+++ week1/Lesson1/Lesson1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            //string s1 = Console.ReadLine();
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = int.Parse(s1);
+
+            //string s = Console.ReadLine();
+            //string[] arr = s.Split();
+            //int a = int.Parse(arr[0]);
+
+            string[] arr = Console.ReadLine().Split(',');
+            int sum = 0;
+            for (int i=0; i < arr.Length; i++)
+            {
+                sum = sum + int.Parse(arr[i]);
+            }
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+        }
+    }
+}
