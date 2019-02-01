fc71e767b3439b69290e97d4e12392ba19630c1a
estharossa <danik.encore@gmail.com>
2019-02-01 16:51:57 +0600

    added comments

--- Week1/task3/Program.cs
+++ Week1/task3/Program.cs
@@ -12,16 +12,16 @@ namespace task3
         {
             int n = int.Parse(Console.ReadLine());
             string s = Console.ReadLine();
-            string[] a = s.Split();
-            List<string> list = new List<string>();
-            foreach(string k in a)
+            string[] a = s.Split(); // creating array from string
+            List<string> list = new List<string>(); // creating the list
+            foreach(string k in a) // using loop to fill the list
             {
-                list.Add(k);
+                list.Add(k); // adding twice to the list
                 list.Add(k);
             }
             foreach(string k in list)
             {
-                Console.Write(k + " ");
+                Console.Write(k + " "); // outputting the list
 
             }
             
