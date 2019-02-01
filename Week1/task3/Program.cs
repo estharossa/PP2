efa286c682ee36c093806b80b24327075725f32c
estharossa <danik.encore@gmail.com>
2019-02-01 16:43:59 +0600

    added tasks 3,4

---
+++ Week1/task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+            string[] a = s.Split();
+            List<string> list = new List<string>();
+            foreach(string k in a)
+            {
+                list.Add(k);
+                list.Add(k);
+            }
+            foreach(string k in list)
+            {
+                Console.Write(k + " ");
+
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
